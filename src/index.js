8c7de635d000ff8db58417551be46df33c7c5406
alla-andronova <allaitcreative@gmail.com>
2021-04-24 21:41:14 +0300

    adds  menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './styles.css';
+import menu from './menu.json';
+import menuTemplate from './templates/menu-items.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitchRef = document.querySelector('#theme-switch-toggle');
+
+if (localStorage.getItem('body-theme') === 'dark-theme') {
+  themeSwitchRef.checked = true;
+  document.querySelector('body').classList.add(Theme.DARK);
+}
+
+themeSwitchRef.addEventListener('change', e => {
+  if (themeSwitchRef.checked) {
+    document.querySelector('body').classList.add(Theme.DARK);
+    localStorage.setItem('body-theme', Theme.DARK);
+  } else {
+    document.querySelector('body').classList.remove(Theme.DARK);
+    localStorage.setItem('body-theme', Theme.LIGHT);
+  }
+});
+
+const menuContainerRef = document.querySelector('.js-menu');
+const menuMarkup = menuTemplate(menu);
+
+menuContainerRef.insertAdjacentHTML('beforeend', menuMarkup);
